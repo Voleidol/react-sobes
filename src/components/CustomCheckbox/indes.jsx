44d8bcc84cad26113b1d14faadab02f22bbe165e
Voleidol <fhdha@yandex.ru>
2025-03-26 15:03:28 +0300

    add logic taskblock

--- src/components/CustomCheckbox/indes.jsx
+++ src/components/CustomCheckbox/indes.jsx
@@ -2,19 +2,22 @@ import React from "react";
 import styles from "./CustomCheckbox.module.scss";
 
 const CustomCheckbox = ({ task, isChecked, setIsChecked, id }) => {
-  
   return (
-    <div className={isChecked ? styles.checkboxWrapperChecked :styles.checkboxWrapper}>
+    <div
+      className={
+        isChecked ? styles.checkboxWrapperChecked : styles.checkboxWrapper
+      }
+    >
       <input
         type="checkbox"
-        id={id}
-        checked={isChecked}
+        id={`checkbox-${id}`}
+        checked={isChecked || false}
         onChange={setIsChecked}
         className={styles.checkboxInput}
       />
-      <label htmlFor={id} className={styles.checkboxLabel}>
+      <label htmlFor={`checkbox-${id}`} className={styles.checkboxLabel}>
         <span className={styles.checkboxCustom}></span>
-        <span className={isChecked ? styles.spanChecked : ''}>{task}</span>
+        <span className={isChecked ? styles.spanChecked : ""}>{task}</span>
       </label>
     </div>
   );
--- src/components/TaskBlock/index.jsx
+++ src/components/TaskBlock/index.jsx
@@ -4,22 +4,50 @@ import styles from "./TaskBlock.module.scss";
 import CustomCheckbox from "../CustomCheckbox/indes";
 
 const TaskBlock = () => {
-
+  const [tasks, setTasks] = useState([
+    "дела",
+    "работа",
+    "хлеб",
+    "молоко",
+    "Евро",
+    "Помидора",
+    "Батон",
+  ]);
   const [isChecked, setIsChecked] = useState({});
-  const [tasks, setTasks] = useState(['дела', 'работа', 'хлеб', 'молоко']);
 
   const addTask = (task) => {
-    setTasks([...tasks, task]);
+    setTasks((prevTasks) => {
+      const uncheckedTasks = prevTasks.filter((t) => !isChecked[t]);
+      const checkedTasks = prevTasks.filter((t) => isChecked[t]);
+      return [task, ...uncheckedTasks, ...checkedTasks];
+    });
+    setIsChecked((prev) => ({ ...prev, [task]: false }));
   };
 
   const deleteTask = (task) => {
-    setTasks(tasks.filter(t => t !== task));
+    setTasks(tasks.filter((elem) => elem !== task));
+    setIsChecked((prev) => {
+      const newChecked = { ...prev };
+      delete newChecked[task];
+      return newChecked;
+    });
   };
 
-  const handleCheckboxChange = (id) => {
-    setIsChecked({
-      ...isChecked,
-      [id]: !isChecked[id],
+  const handleCheckboxChange = (id, task) => {
+    setIsChecked((prev) => {
+      const newCheckedState = { ...prev, [task]: !prev[task] };
+
+      // Перемещаем выполненные задачи в конец
+      const sortedTasks = [...tasks].sort((a, b) =>
+        newCheckedState[a] === newCheckedState[b]
+          ? 0
+          : newCheckedState[a]
+          ? 1
+          : -1
+      );
+
+      setTasks(sortedTasks);
+      return newCheckedState;
     });
   };
 
@@ -29,10 +57,10 @@ const TaskBlock = () => {
         <AddTask addTask={addTask} />
 
         {tasks.map((task, i) => (
-          <div className={styles.textTask}>
+          <div className={styles.textTask} key={i}>
             <CustomCheckbox
-              isChecked={isChecked[i]}
-              setIsChecked={()=> handleCheckboxChange(i)}
+              isChecked={!!isChecked[task]}
+              setIsChecked={() => handleCheckboxChange(i, task)}
               task={task}
               id={i}
             />
