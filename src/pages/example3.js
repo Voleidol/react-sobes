73ced21c0b2093b9ae157f7a8e43b6ead6c447c1
Roman <fhdha@yandex.ru>
2025-04-12 01:16:29 +0300

    add

---
+++ src/pages/example3.js
@@ -0,0 +1,29 @@
+// 1.
+// const arr = [1, 2, 3, 4, 5];
+// const newArr = arr.reduce((s, cur) => s + cur, 0)
+// console.log(newArr)
+
+// 2. как реализовать при помощи reduce??
+// const arr = [5, 3, 9, 1];
+// const newArrMin = Math.min(...arr)
+// const newArrMax = Math.max(...arr)
+// console.log(newArrMin)
+// console.log(newArrMax)
+
+// 3.
+// const newVal = [-2, 5, 0, -1, 6];
+// const newValCurrent = newVal.filter((item) => item > 0)
+// console.log(newValCurrent);
+
+// 4. не сделал!!!
+// const arr = ['apple', 'banana', 'apple', 'orange', 'banana'];
+
+// 5. 
+// const arr = [1, 2, 2, 3, 4, 4];
+// const newSet = new Set(arr);
+// const newArr = [...newSet];
+// console.log(newArr)
+
+// или
+const arr = [1, 2, 2, 3, 4, 4];
+
